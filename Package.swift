4b186763fae77fd4ccb1d22d9316defe6b0ddc25
Omar Albeik <omaralbeik@gmail.com>
2026-07-31 20:52:59 +0200

    Release v1.1.0-beta.1

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "libvlc",
-      url: "https://github.com/harflabs/SwiftVLC/releases/download/v1.0.0/libvlc.xcframework.zip",
-      checksum: "23509b945aafb97634d2e66af24a377d7bd50b672aba8f5dec1d6f9ef8614045"
+      url: "https://github.com/harflabs/SwiftVLC/releases/download/v1.1.0-beta.1/libvlc.xcframework.zip",
+      checksum: "03a57454a6159c455406889c7867e0b284db028d2734a10bdf85a6a7285c862f"
     ),
     .target(
       name: "CLibVLC",
